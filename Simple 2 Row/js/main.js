666f1b0e72164215b7ea107a25f0c5f702a1249b
Adi Vrskic <adivrskic123@gmail.com>
2021-08-27 16:40:49 -0400

    Work on 2 row

--- Simple 2 Row/js/main.js
+++ Simple 2 Row/js/main.js
@@ -1,33 +1,58 @@
-var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
-var date = new Date();
-var n = month[date.getMonth()];
-var day_of_week = day[date.getDay()];
-var day_of_month = date.getDate();
-var hours = date.getHours();
+const date = new Date();
+const n = month[date.getMonth()];
+const day_of_week = day[date.getDay()];
+const day_of_month = date.getDate();
+const hours = date.getHours();
 
-function nth(n){return["st","nd","rd"][((n+90)%100-10)%10-1]||"th"}
+const nth = (n) => {
+  return ["st", "nd", "rd"][((((n + 90) % 100) - 10) % 10) - 1] || "th";
+};
 
-function checkTime(i) {
+const checkTime = (i) => {
   if (i < 10) {
     i = "0" + i;
   }
   return i;
-}
+};
 
-function startTime() {
+const startTime = () => {
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
-  document.getElementById('time').innerHTML = h + " : " + m + " : " + s;
-  t = setTimeout(function() {
-    startTime()
+  document.getElementById("time").innerHTML = h + " : " + m + " : " + s;
+  t = setTimeout(function () {
+    startTime();
   }, 500);
-}
+};
 startTime();
 
-document.getElementById("date").innerHTML = day_of_week + ', ' + day_of_month + nth(day_of_month) + ' of ' + n;
+document.getElementById("date").innerHTML =
+  day_of_week + ", " + day_of_month + nth(day_of_month) + " of " + n;
